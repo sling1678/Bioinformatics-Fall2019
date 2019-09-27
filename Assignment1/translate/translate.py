f17296cb4c1e7397aea772a39284a35a2659f2c3
atr6456 <atr6456@truman.edu>
2019-09-26 20:35:29 -0500

    update plot with shaded area

--- Assignment1/translate/translate.py
+++ Assignment1/translate/translate.py
@@ -6,17 +6,17 @@ def translate_simple(mRNA):
   Assumes given mRNA starts with a start codon.
   Parameters:
     mRNA : string consisting of AUGC and multiple of 3
-    one_letter : set to True if want one-letter, default False
   Returns:
     aminoacid_sequence : get one-letter sequence; places '*' where there is codon error.
     nucleotide_errors : List of tuple (index of mRNA sequence where a 3-nucleotide is not in the dictionary of codes
+    stop_codon: boolean variable for if the gene contains a stop codon
   """
   code = genetic_code.genetic_code()
   
   assert(len(mRNA)%3 == 0), "mRNA must have multiples of 3 nucleotides"
   aminoacid_sequence = ""
   nucleotide_errors = []
-  for i in range(0,len(mRNA), 3):
+  for i in range(0, len(mRNA), 3):
     try:
       aminoacid_sequence += code[mRNA[i:i+3]]
     except KeyError:
@@ -26,3 +26,4 @@ def translate_simple(mRNA):
       break
 
   return aminoacid_sequence[:-1], nucleotide_errors
+
